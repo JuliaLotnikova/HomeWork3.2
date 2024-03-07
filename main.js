3c908584c2ea8e22f663d5faf3936f0c73bb8ec8
JuliaLotnikova <150443868+JuliaLotnikova@users.noreply.github.com>
2024-03-07 14:23:17 +0300

    v1

---
+++ main.js
@@ -0,0 +1,16 @@
+let fruit1 = document.getElementById("fruit1").textContent;
+let fruit2 = document.getElementById("fruit2").textContent;
+let fruit3 = document.getElementById("fruit3").textContent;
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+
+document.getElementById("fruit1").textContent = "морковь";
+document.getElementById("fruit2").textContent = "помидор";
+document.getElementById("fruit3").textContent = "огурец";
+
+document.getElementById("block1").textContent = "- Это яблоко или морковь?";
+document.getElementById("block2").textContent = "- Не знаю";
+document.getElementById("block3").textContent = "- Может посмотреть в консоли?";
+
